6238f03874c40708d476ad2d82ab71c61e0e855c
Amanda Leaders <alsleaders@gmail.com>
2019-06-07 20:24:45 -0400

    working on map of array

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,17 @@ import axios from 'axios'
 const COMPANYNAME = 'Leaders Imagined'
 const API_URL = 'https://sdg-staff-directory-app.herokuapp.com/api'
 export default function Home() {
+  const [employeeList, setEmployeeList] = useState([])
+
   useEffect(() => {
-    axios
-      .get(`${API_URL}/${COMPANYNAME}/employees`)
-      .then(resp => {
-        console.log(resp)
-        return resp.json
-      })
-      .then(person => {
-        console.log(person)
-        return person
-      })
+    axios.get(`${API_URL}/${COMPANYNAME}/employees`).then(resp => {
+      console.log(resp.data)
+      setEmployeeList(resp.data)
+    })
+    // .then(person => {
+    //   console.log(person)
+    //   return person.data.data
+    // })
   })
 
   // gonna need a Post for new employees
@@ -22,6 +22,15 @@ export default function Home() {
   return (
     <>
       <h1>Hello, Minions!</h1>
+      <ul>
+        {employeeList.map(person => {
+          return (
+            <li>
+              <PFC key={person.id} />
+            </li>
+          )
+        })}
+      </ul>
     </>
   )
 }
